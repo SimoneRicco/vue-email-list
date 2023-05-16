7a80db94174a8723a509a193637ce87ba9aa2df1
SimoneRicco <smnrcc0@gmail.com>
2023-05-16 16:06:21 +0200

    bug undefined

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            emailList: "",
+            emailList: [],
             emailNumber: 10
         }
     },
     methods: {
-        getEmails(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(ServerResponse => this.emailList = ServerResponse.response);
-            // console.log()
+        getEmails() {
+            for (let i = 0; i < this.emailNumber; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(ServerResponse => this.emailList[i] += ServerResponse.data.response);
+            }
         }
     },
     created() {
-        console.log(this.getEmails())    
+        console.log(this.getEmails())
     },
 });
 app.mount("#app");
